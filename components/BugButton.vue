38592e388301e4d6d563bfe923e37495007ce469
M.Palerme <marcellino.palerme@inrae.fr>
2024-01-19 14:50:55 +0100

    #39 comment BugButton component

--- components/BugButton.vue
+++ components/BugButton.vue
@@ -3,8 +3,9 @@ SPDX-FileCopyrightText: 2024 Marcellino Palerme <marcellino.palerme@inrae.fr>
 
 SPDX-License-Identifier: MIT
 -->
-
+<!-- Button to send email if we have a bug -->
 <template>
+  <!-- Button from NuxtUI -->
   <UButton
     to="mailto:p2m2-it@inrae.fr?subject=retour tamama (ep2m2)"
     target="_blank"
