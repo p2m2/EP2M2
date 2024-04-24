9fc05156b357d24802ea121ecf8a2088a14c684a
M.Palerme <marcellino.palerme@inrae.fr>
2024-04-24 11:31:54 +0200

    #41 tab active about page

--- layouts/navigateLayout.vue
+++ layouts/navigateLayout.vue
@@ -9,23 +9,31 @@ SPDX-License-Identifier: MIT
     This layout provide to move between pages
  -->
 <script setup lang="ts">
-import { string } from 'valibot';
-
+// Get translate
 const {t} = useI18n();
 const router = useRouter();
 // Get name of all pages
-const listPages = router.getRoutes().map(x => x.name) as string[];
+const listPages = router.getRoutes().map(x => x.name).sort() as string[];
 //  Get name of current page
 const currentPage = router.currentRoute.value.name;
+//  Get index of page to indicate to tabs which tab highlining
+const currentIndex = ref(listPages.indexOf(currentPage as string))
 
+/**
+ * Function to change page. We manage case of page project.
+ * This page has Index as name but to go this page the path
+ * is /
+ * @param namePage string Name of page
+ */
 function goToTab(namePage:string){
-
   let realPage = '/';
 
+  // Check if we are in project page case
   if(namePage != 'index'){
     realPage = '/' + namePage;
   }
 
+  // move to page
   navigateTo(realPage);
 }
 </script>
@@ -34,29 +42,33 @@ function goToTab(namePage:string){
   <!-- Show this layout in default layout -->
   <NuxtLayout name="default">
     <v-container>
-      <v-card variant="plain">
-        <!-- Navigation part -->
-        <v-tabs bg-color="primary"> 
-          <!-- Create one tab by page. When we click on we open the page -->
-          <v-tab
-            v-for="(pageName, index) in listPages.sort()"             
-            :key="index"
-            :value="pageName"
-            @click="goToTab(pageName)"
-          >
-            <!-- Name of tab -->
-            {{ t("page." + pageName) }} 
-          </v-tab>
-        </v-tabs>
-        <v-card-text>
-          <v-window>
-            <!-- We show only tab contain of current page -->
-            <v-window-item :value="currentPage">
-              <slot />
-            </v-window-item>
-          </v-window>
-        </v-card-text>
-      </v-card>
+      <!-- Navigation part -->
+      <v-row>
+        <v-col>
+          <v-tabs 
+            v-model="currentIndex"
+            bg-color="primary"
+            fixed-tabs
+          > 
+            <!-- Create one tab by page. When we click on we open the page -->
+            <v-tab
+              v-for="(pageName, index) in listPages"            
+              :key="index"
+              :value="pageName"
+              @click="goToTab(pageName)"
+            >
+              <!-- Name of tab --> 
+              {{ t("page." + pageName) }}
+            </v-tab>
+          </v-tabs>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <!-- part to show page -->
+          <slot />
+        </v-col>
+      </v-row>
     </v-container>
   </NuxtLayout>
 </template>
