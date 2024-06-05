927ab8308f7eb6b16959f6b09baadbc24f9fda9c
M.Palerme <marcellino.palerme@inrae.fr>
2024-06-05 16:09:43 +0200

    #42 write test for serie page

--- test/pages/serie.test.ts
+++ test/pages/serie.test.ts
@@ -3,36 +3,220 @@
 // SPDX-License-Identifier: MIT
 
 import { describe, test } from "vitest";
-import { mount } from "@vue/test-utils";
 import serie from "~/pages/serie.vue";
-import useVuetify from "../extra/useVuetify";
-import { mockComponent } from '@nuxt/test-utils/runtime';
+import { mockComponent,mountSuspended } from '@nuxt/test-utils/runtime';
+import { flushPromises } from "@vue/test-utils";
 
+// Function to mock a component
+function mockComponentName(name: string) {
+    return defineComponent({
+        setup() {
+            return () => h('div', { class: name })
+        }
+    })
+}
 // mock the layout
 // Layout is empty
 mockComponent('NuxtLayout', () => import("../extra/EmptyComponent.vue"));
+mockComponent('ManageSerieAsync', () => mockComponentName('ManageSerieAsync'));
+mockComponent('ManageMachineAsync', () => mockComponentName('ManageMachineAsync'));
+mockComponent('ManageBaseAsync', () => mockComponentName('ManageBaseAsync'));
+mockComponent('ManageMixAsync', () => mockComponentName('ManageMixAsync'));
+mockComponent('ManageMoleculeAsync', () => mockComponentName('ManageMoleculeAsync'));
+mockComponent('ManageMotherAsync', () => mockComponentName('ManageMotherAsync'));
 
+// function to check if the component is loaded
+function checkLoadComponent(wrapper: any, 
+                            contain: string[], notContain: string[]) {
+    const html = wrapper.html();
+    contain.forEach((element) => {
+        expect(html).toContain(element);
+    });
+    notContain.forEach((element) => {
+        expect(html).not.toContain(element);
+    });
+}
+
+// Fucntion check get great component activated
+function checkActivated (wrapper: any, activated: string) {
+    
+    expect(wrapper.findAll(`.v-window-item--active`)).toHaveLength(1);
+    const tabActivated = wrapper.find(`.v-window-item--active`);
+    expect(tabActivated.html()).toContain(activated);
+}
 
 describe("serie page", () =>{
-    test.todo('showed default tab of serie page is serie tab', () => {
-        const wrapper = mount(serie,{
-            global:{
-                plugins:[useVuetify()],
-                
-            }
-        });
+    test('showed default tab of serie page is serie tab', async() => {
 
-        expect(wrapper.findComponent("ManageSerieAsync").exists())
-            .toBeTruthy();
-        expect(wrapper.findComponent("ManageMachineAsync").exists())
-            .toThrowError();
-        expect(wrapper.findComponent("ManageBaseAsync").exists())
-            .toThrowError();
-        expect(wrapper.findComponent("ManageMixAsync").exists())
-            .toThrowError();
-        expect(wrapper.findComponent("ManageMoleculAsync").exists())
-            .toThrowError();
-        expect(wrapper.findComponent("ManageMotherAsync").exists())
-            .toThrowError();
+        const wrapper = await mountSuspended(serie);
+        
+        checkLoadComponent(wrapper, 
+              ["ManageSerieAsync"],
+              ["ManageMachineAsync", "ManageBaseAsync", "ManageMixAsync", "ManageMoleculeAsync", "ManageMotherAsync"]);
+
+        checkActivated(wrapper, "ManageSerieAsync");
+        
+    });
+
+    test('load machine tab after a click', async() => {
+
+        const wrapper = await mountSuspended(serie);
+        
+        checkLoadComponent(wrapper,
+                ["ManageSerieAsync"],
+                ["ManageMachineAsync", "ManageBaseAsync", "ManageMixAsync", "ManageMoleculeAsync", "ManageMotherAsync"]);
+        
+        checkActivated(wrapper, "ManageSerieAsync");
+
+        const bt = wrapper.find(`[value="machine"]`);
+        
+        await bt.trigger('click');
+        await flushPromises();
+
+        checkLoadComponent(wrapper,
+              ["ManageSerieAsync", "ManageMachineAsync"],
+              ["ManageBaseAsync", "ManageMixAsync", "ManageMoleculeAsync", "ManageMotherAsync"]);
+        
+        checkActivated(wrapper, "ManageMachineAsync");
+    });
+
+    test('load machine tab after two click', async() => {
+
+        const wrapper = await mountSuspended(serie);
+        checkLoadComponent(wrapper,
+            ["ManageSerieAsync"],
+            ["ManageMachineAsync", "ManageBaseAsync", "ManageMixAsync", "ManageMoleculeAsync", "ManageMotherAsync"]);
+
+        checkActivated(wrapper, "ManageSerieAsync");
+
+        await wrapper.find(`[value="machine"]`).trigger('click');
+        await flushPromises();
+        await wrapper.find(`[value="mix"]`).trigger('click');
+        await flushPromises();
+
+        checkLoadComponent(wrapper,
+            ["ManageSerieAsync", "ManageMachineAsync", "ManageMixAsync"],
+            ["ManageBaseAsync", "ManageMoleculeAsync", "ManageMotherAsync"]);
+
+        checkActivated(wrapper, "ManageMixAsync");
+
     })
-});
+
+    test('load machine tab after three click', async() => {
+
+        const wrapper = await mountSuspended(serie);
+
+        checkLoadComponent(wrapper,
+            ["ManageSerieAsync"],
+            ["ManageMachineAsync", "ManageBaseAsync", "ManageMixAsync", "ManageMoleculeAsync", "ManageMotherAsync"]);
+
+        checkActivated(wrapper, "ManageSerieAsync");
+
+        await wrapper.find(`[value="machine"]`).trigger('click');
+        await flushPromises();
+        await wrapper.find(`[value="molecule"]`).trigger('click');
+        await flushPromises();
+        await wrapper.find(`[value="base"]`).trigger('click');
+        await flushPromises();
+
+        checkLoadComponent(wrapper,
+              ["ManageSerieAsync", "ManageMachineAsync", "ManageMoleculeAsync",
+               "ManageBaseAsync"],
+              ["ManageMixAsync", "ManageMotherAsync"]);
+        
+        checkActivated(wrapper, "ManageBaseAsync");
+
+    })
+
+    test('load machine tab after four click', async() => {
+
+        const wrapper = await mountSuspended(serie);
+
+        checkLoadComponent(wrapper,
+            ["ManageSerieAsync"],
+            ["ManageMachineAsync", "ManageBaseAsync", "ManageMixAsync", "ManageMoleculeAsync", "ManageMotherAsync"]);
+
+        checkActivated(wrapper, "ManageSerieAsync");
+
+        await wrapper.find(`[value="mother"]`).trigger('click');
+        await flushPromises();
+        await wrapper.find(`[value="mix"]`).trigger('click');
+        await flushPromises();
+        await wrapper.find(`[value="base"]`).trigger('click');
+        await flushPromises();
+        await wrapper.find(`[value="molecule"]`).trigger('click');
+        await flushPromises();
+
+        checkLoadComponent(wrapper,
+              ["ManageSerieAsync", "ManageMoleculeAsync",
+               "ManageBaseAsync", "ManageMixAsync", "ManageMotherAsync"],
+              ["ManageMachineAsync"]);
+    
+        checkActivated(wrapper, "ManageMoleculeAsync");
+    })
+
+    test('load machine tab after click all tab', async() => {
+
+        const wrapper = await mountSuspended(serie);
+
+        checkLoadComponent(wrapper,
+            ["ManageSerieAsync"],
+            ["ManageMachineAsync", "ManageBaseAsync", "ManageMixAsync", "ManageMoleculeAsync", "ManageMotherAsync"]);
+
+        checkActivated(wrapper, "ManageSerieAsync");
+
+        await wrapper.find(`[value="mother"]`).trigger('click');
+        await flushPromises();
+        await wrapper.find(`[value="mix"]`).trigger('click');
+        await flushPromises();
+        await wrapper.find(`[value="machine"]`).trigger('click');
+        await flushPromises();
+        await wrapper.find(`[value="base"]`).trigger('click');
+        await flushPromises();
+        await wrapper.find(`[value="molecule"]`).trigger('click');
+        await flushPromises();
+
+        checkLoadComponent(wrapper,
+              [ "ManageSerieAsync", "ManageMoleculeAsync","ManageMachineAsync",
+               "ManageBaseAsync", "ManageMixAsync", "ManageMotherAsync"],
+              []);
+
+        checkActivated(wrapper, "ManageMoleculeAsync");
+
+    })
+
+    test('click a second time a tab', async() => {
+
+        const wrapper = await mountSuspended(serie);
+
+        checkLoadComponent(wrapper,
+            ["ManageSerieAsync"],
+            ["ManageMachineAsync", "ManageBaseAsync", "ManageMixAsync", "ManageMoleculeAsync", "ManageMotherAsync"]);
+
+        checkActivated(wrapper, "ManageSerieAsync");
+
+        await wrapper.find(`[value="machine"]`).trigger('click');
+        await flushPromises();
+        await wrapper.find(`[value="molecule"]`).trigger('click');
+        await flushPromises();
+        await wrapper.find(`[value="base"]`).trigger('click');
+        await flushPromises();
+
+        checkLoadComponent(wrapper,
+              ["ManageSerieAsync", "ManageMachineAsync", "ManageMoleculeAsync",
+               "ManageBaseAsync"],
+              ["ManageMixAsync", "ManageMotherAsync"]);
+
+        checkActivated(wrapper, "ManageBaseAsync");
+
+        await wrapper.find(`[value="molecule"]`).trigger('click');
+        await flushPromises();
+
+        checkLoadComponent(wrapper,
+            ["ManageSerieAsync", "ManageMachineAsync", "ManageMoleculeAsync",
+             "ManageBaseAsync"],
+            ["ManageMixAsync", "ManageMotherAsync"]);
+
+        checkActivated(wrapper, "ManageMoleculeAsync");
+    })
+}); 
