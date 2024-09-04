e17e084b7fe42e61094001f25945562a42783efc
M.Palerme <marcellino.palerme@inrae.fr>
2024-09-04 10:46:30 +0200

    #48 calculate ratio of function ax+b. We use all calibration curves of project

--- server/api/function/RegCalibCurve.ts
+++ server/api/function/RegCalibCurve.ts
@@ -8,38 +8,38 @@
 import pg from "pg";
 import { linearRegression } from "simple-statistics";
 
-export async function calculateRatioCalibCurve(idCalibCurve:string):Promise<number | {[key:string]:number}>{
+export async function calculateRatioCalibCurve(arrayIdCalibCurve: string[]): Promise<number | { [key: string]: number }> {
 
     const client = new pg.Client();
-
+    const idCalibCurve = arrayIdCalibCurve.join("','");
     return client.connect()
         .then(() => {
             // Get the area and concentration of the metabolite of the calibration curve
             return client.query(`
-                SELECT id_mol,area, concentration FROM daughter
-                WHERE id_calib_curves='${idCalibCurve}'
+                SELECT id_mol,area, concentration
+                FROM daughter
+                WHERE id_calib_curves IN ('${idCalibCurve}')
             `)
         })
-        .then((res:{rows:any[]}) => {
+        .then((res: { rows: any[] }) => {
             // create array area/concentration for each metabolite
-            const metaConcentration: {[key:string]:[number,number][]} = {}
-            
-            for(const row of res.rows){
-                // if the metabolite is not in the array, create it
-                if(metaConcentration[row.id_mol]===undefined){
-                    // initialize first element to position 0,0
-                    // if no metabolits, machine return noting
-                    metaConcentration[row.id_mol] = [[0,0]]
+            const metaConcentration: { [key: string]: [number, number][] } = {};
+            // Group the area and concentration of each metabolite
+            res.rows.map((row) => {metaConcentration[row.id_mol].push([row.area, row.concentration])});
+            // add the first element to 0,0 if we have one point for the 
+            //   metabolite
+            for (const key in metaConcentration) {
+                if (metaConcentration[key].length === 1) {
+                    metaConcentration[key].push([0, 0]);
                 }
-                metaConcentration[row.id_mol].push([row.area,row.concentration]);
             }
-            
-            // caclulate director coefficient of each metabolite
-            const metaRatio: {[key:string]:number}= {}
-            for(const key in metaConcentration){
+
+            // caclulate regression of each metabolite in ax + b
+            const metaRatio: { [key: string]: {a: number, b: number} } = {}
+            for (const key in metaConcentration) {
                 // calculate the linear regression
-                const {m} = linearRegression(metaConcentration[key]);
-                metaRatio[key] = m;
+                const { m, b } = linearRegression(metaConcentration[key]);
+                metaRatio[key] = {a: m, b: b};
             }
             return metaRatio
         })
