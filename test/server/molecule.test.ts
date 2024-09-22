b5b00b7e4cb835b687b842752d4558c04dcf6668
M.Palerme <marcellino.palerme@inrae.fr>
2024-09-22 22:47:42 +0200

    #57 correct key

--- test/server/molecule.test.ts
+++ test/server/molecule.test.ts
@@ -2,7 +2,8 @@
 //
 // SPDX-License-Identifier: MIT
 
-import { describe, test, vi } from "vitest";
+import {addMolecule, updateMolecule, type tMolecule, } from "~/server/api/molecule/functions";
+import { expect, test, describe, vi } from 'vitest';
 
 // mock call to database
 const mPart = { query: vi.fn() };
@@ -18,14 +19,11 @@ describe("molecule", async () => {
     test('add molecule', async () => {
         mPart.query.mockResolvedValue({ rows: [{ id: 0 }] });
 
-        const result = await $fetch("api/molecule/add", {
-            method: "POST",
-            body: JSON.stringify({
+        const result = await addMolecule({
                 name: "voglibose",
                 formula: "C10H21NO7",
                 mass: 267.277
-            })
-        });
+            } as tMolecule)
         expect(result).toBe(0);
         expect(mPart.query).toHaveBeenCalledWith("INSERT INTO molecule (name, formula, mass) VALUES ($1, $2, $3) RETURNING id", ["voglibose", "C10H21NO7", 267.277]);
 
@@ -34,34 +32,18 @@ describe("molecule", async () => {
     test('add molecule with eqivalent and synonym', async () => {
         mPart.query.mockResolvedValue({ rows: [{ id: 1 }] });
 
-        const result = await $fetch("api/molecule/add", {
-            method: "POST",
-            body: JSON.stringify({
+        const result = await addMolecule({
                 name: "voglibose",
                 formula: "C10H21NO7",
                 mass: 267.277,
-                equivalent: [0],
-                synonym: ["vog"]
+                equivalents: [0],
+                synonyms: ["vog"]
             })
-        });
         expect(result).toBe(0);
         expect(mPart.query).toHaveBeenCalledWith("INSERT INTO molecule (name, formula, mass) VALUES ($1, $2, $3) RETURNING id", ["voglibose", "C10H21NO7", 267.277]);
         expect(mPart.query).toHaveBeenCalledWith("INSERT INTO equivalent (id_mol_0, id_mol_1) VALUES ($1, $2)", [1, 0]);
         expect(mPart.query).toHaveBeenCalledWith("INSERT INTO synonym (id_mol, name) VALUES ($1, $2)", [1, "vog"]);
 
-    });
-
-    test('delete molecule', async () => {
-        mPart.query.mockResolvedValue(0);
-
-        const result = await $fetch("api/molecule/delete", {
-            method: "POST",
-            body: JSON.stringify({
-                id: 0
-            })
-        });
-        expect(result).toBe(0);
-        expect(mPart.query).toHaveBeenCalledWith("DELETE FROM molecule WHERE id = $1", [0]);
     });
 
     test('modif molecule: add synonym', async () => {
@@ -74,17 +56,14 @@ describe("molecule", async () => {
             synonym: ["vague"]
         }] });
 
-        const result = await $fetch("api/molecule/modif", {
-            method: "POST",
-            body: JSON.stringify({
-                id: 1,
+        const result = updateMolecule({
+            id: 1,
             name: "voglibose",
             formula: "C10H21NO7",
             mass: 267.277,
-            equivalent: [0],
-            synonym: ["vague", "vog"]
+            equivalents: [0],
+            synonyms: ["vague", "vog"]
             })
-        });
         expect(result).toBe(0);
         expect(mPart.query).toHaveBeenCalledWith("SELECT * FROM view_complete_molecule WHERE id = $1", [1]);
         expect(mPart.query).toHaveBeenCalledWith("INSERT INTO synonym (id_mol, name) VALUES ($1, $2)", [1, "vog"]);
@@ -100,17 +79,14 @@ describe("molecule", async () => {
             synonym: ["vague", "vog"]
         }] });
 
-        const result = await $fetch("api/molecule/modif", {
-            method: "POST",
-            body: JSON.stringify({
-                id: 1,
+        const result = await updateMolecule({
+            id: 1,
             name: "voglibose",
             formula: "C10H21NO7",
             mass: 267.277,
-            equivalent: [0],
-            synonym: ["vague"]
+            equivalents: [0],
+            synonyms: ["vague"]
             })
-        });
         expect(result).toBe(0);
         expect(mPart.query).toHaveBeenCalledWith("SELECT * FROM view_complete_molecule WHERE id = $1", [1]);
         expect(mPart.query).toHaveBeenCalledWith("DELETE FROM synonym WHERE id_mol = $1 AND name = $2", [1, "vog"]);
@@ -126,17 +102,14 @@ describe("molecule", async () => {
             synonym: ["vague"]
         }] });
 
-        const result = await $fetch("api/molecule/modif", {
-            method: "POST",
-            body: JSON.stringify({
-                id: 1,
+        const result = await updateMolecule({
+            id: 1,
             name: "voglibose",
             formula: "C10H21NO7",
             mass: 267.277,
-            equivalent: [0, 2],
-            synonym: ["vague"]
+            equivalents: [0, 2],
+            synonyms: ["vague"]
             })
-        });
         expect(result).toBe(0);
         expect(mPart.query).toHaveBeenCalledWith("SELECT * FROM view_complete_molecule WHERE id = $1", [1]);
         expect(mPart.query).toHaveBeenCalledWith("INSERT INTO equivalent (id_mol_0, id_mol_1) VALUES ($1, $2)", [1, 2]);
@@ -152,17 +125,14 @@ describe("molecule", async () => {
             synonym: ["vague"]
         }] });
 
-        const result = await $fetch("api/molecule/modif", {
-            method: "POST",
-            body: JSON.stringify({
-                id: 1,
+        const result = await updateMolecule({
+            id: 1,
             name: "voglibose",
             formula: "C10H21NO7",
             mass: 267.277,
-            equivalent: [0],
-            synonym: ["vague"]
+            equivalents: [0],
+            synonyms: ["vague"]
             })
-        });
         expect(result).toBe(0);
         expect(mPart.query).toHaveBeenCalledWith("SELECT * FROM view_complete_molecule WHERE id = $1", [1]);
         expect(mPart.query).toHaveBeenCalledWith("DELETE FROM equivalent WHERE id_mol_0 = $1 AND id_mol_1 = $2 OR id_mol_0 = $2 AND id_mol_1 = $1", [1, 2]);
