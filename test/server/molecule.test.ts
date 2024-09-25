478c485c8e7cd4792e8cbd1b986b3062bb58d62f
M.Palerme <marcellino.palerme@inrae.fr>
2024-09-25 08:58:02 +0200

    #57 Test check we don't call database with bad query

--- test/server/molecule.test.ts
+++ test/server/molecule.test.ts
@@ -2,25 +2,16 @@
 //
 // SPDX-License-Identifier: MIT
 
-import {addMolecule, updateMolecule, type tMolecule, } from "~/server/api/molecule/functions";
-import { expect, test, describe, vi } from 'vitest';
-import {config } from '@vue/test-utils';
+import { addMolecule, updateMolecule, type tMolecule, } from "~/server/api/molecule/functions";
+import { expect, test, describe, vi, type Mock } from 'vitest';
+import { config } from '@vue/test-utils';
+import { queryDatabase } from "~/server/api/function/database";
 
-// mock call to database
 
-// Mock confirm box
-const mockQuery = vi.fn(()=> console.log("plouf"));
-vi.stubGlobal('queryDatabase',mockQuery);
-
-// const mPart = { query: vi.fn() };
-// vi.mock('pg', async( importOriginal) => {
-//     const actual = await importOriginal()
-//     return {
-//         ...actual,
-//         Pool: vi.fn(() => mPart),
-//         Client: vi.fn(() => mPart)
-//     };
-// });
+// Mock queryDatabase
+vi.mock('~/server/api/function/database', () => ({
+    queryDatabase: vi.fn()
+}));
 
 describe("molecule", async () => {
     beforeEach(async () => {
@@ -29,71 +20,150 @@ describe("molecule", async () => {
         vi.resetAllMocks();
     })
     test('add molecule', async () => {
-        // mockQuery.mockResolvedValue({ rows: [{ id: 0 }] });
 
+        (queryDatabase as Mock).mockResolvedValueOnce({ rows: [{ id: 1 }] });
+        (queryDatabase as Mock).mockImplementation((query: string,
+                                                    values: any[]) => {
+            if (query.includes("INSERT INTO") && query.includes("molecule")) {
+                values.map((val) => expect(["voglibose", "C10H21NO7", 267.277]).toContain(val));
+                ["name", "formula", "mass"].map((inc) => 
+                    expect(query.includes(inc)).toBe(true)
+                );
+                
+            }
+            else{
+                assert.fail("Call to queryDatabase with unexpected query " + query);
+            }
+        });
         const result = await addMolecule({
-                id:null,
-                name: "voglibose",
-                formula: "C10H21NO7",
-                mass: 267.277,
-                equivalents:[],
-                synonyms:[]
-            } as tMolecule)
-        // expect(result).toBe(0);
-        // expect(mockQuery).toHaveBeenCalledWith("INSERT INTO molecule (name, formula, mass) VALUES ($1, $2, $3) RETURNING id", ["voglibose", "C10H21NO7", 267.277]);
-
+            id: null,
+            name: "voglibose",
+            formula: "C10H21NO7",
+            mass: 267.277,
+            equivalents: [],
+            synonyms: []
+        } as tMolecule)
+        expect(result).toBe(0);
     });
 
     test('add molecule with eqivalent and synonym', async () => {
-        mockQuery.mockResolvedValue({ rows: [{ id: 1 }] });
+
+        (queryDatabase as Mock).mockResolvedValueOnce({ rows: [{ id: 1 }] });
+        (queryDatabase as Mock).mockResolvedValueOnce({ rows: [] });
+        (queryDatabase as Mock).mockResolvedValueOnce({ rows: [] });
+
+        (queryDatabase as Mock).mockImplementation((query: string,
+                                                    values: any[]) => {
+            if (query.includes("INSERT INTO") && query.includes("molecule")) {
+                values.map((val) => expect(["voglibose", "C10H21NO7", 267.277]).toContain(val));
+                ["name", "formula", "mass"].map((inc) => 
+                    expect(query.includes(inc)).toBe(true)
+                );
+            }
+            else if (query.includes("INSERT INTO") && query.includes("equivalent")) {
+                values.map((val) => expect([1,0]).toContain(val));
+                ["id_mol_0", "id_mol_1"].map((inc) => 
+                    expect(query.includes(inc)).toBe(true)
+                );
+            }
+            else if (query.includes("INSERT INTO") && query.includes("synonym")) {
+                values.map((val) => expect([1, "vog"]).toContain(val));
+                ["id_mol", "name"].map((inc) => 
+                    expect(query.includes(inc)).toBe(true)
+                );
+            }
+            else{
+                assert.fail("Call to queryDatabase with unexpected query " + query);
+            }
+        });
 
         const result = await addMolecule({
-                name: "voglibose",
-                formula: "C10H21NO7",
-                mass: 267.277,
-                equivalents: [0],
-                synonyms: ["vog"]
-            })
+            name: "voglibose",
+            formula: "C10H21NO7",
+            mass: 267.277,
+            equivalents: [0],
+            synonyms: ["vog"]
+        } as unknown as tMolecule)
         expect(result).toBe(0);
-        expect(mockQuery).toHaveBeenCalledWith("INSERT INTO molecule (name, formula, mass) VALUES ($1, $2, $3) RETURNING id", ["voglibose", "C10H21NO7", 267.277]);
-        expect(mockQuery).toHaveBeenCalledWith("INSERT INTO equivalent (id_mol_0, id_mol_1) VALUES ($1, $2)", [1, 0]);
-        expect(mockQuery).toHaveBeenCalledWith("INSERT INTO synonym (id_mol, name) VALUES ($1, $2)", [1, "vog"]);
 
     });
 
     test('modif molecule: add synonym', async () => {
-        mockQuery.mockResolvedValueOnce({ rows: [{ 
-            id: 1,
-            name: "voglibose",
-            formula: "C10H21NO7",
-            mass: 267.277,
-            equivalent: [0],
-            synonym: ["vague"]
-        }] });
+        (queryDatabase as Mock).mockResolvedValueOnce({
+                    rows: [{
+                        id: 1,
+                        name: "voglibose",
+                        formula: "C10H21NO7",
+                        mass: 267.277,
+                        equivalent: [0],
+                        synonym: ["vague"]
+                    }]
+        });
+        (queryDatabase as Mock).mockResolvedValueOnce({ rows: [] });
+        (queryDatabase as Mock).mockResolvedValueOnce({ rows: [] });
 
-        const result = updateMolecule({
+        (queryDatabase as Mock).mockImplementation((query: string,
+                                                    values: any[]) => {
+            if (query.includes("SELECT") 
+                && query.includes("FROM view_complete_molecule") 
+                && query.includes("WHERE id = $1")) {
+                expect(values).toContain([1]);
+
+            }
+            else if (query.includes("DELETE FROM") && query.includes("synonym")) {
+                expect(values).toContain([1]);
+                expect(query.includes("WHERE id_molecule = $1")).toBe(true)
+            }
+            else if (query.includes("INSERT INTO") && query.includes("synonym")) {
+                values.map((val) => expect([1, "vog"]).toContain(val));
+                ["id_mol", "name"].map((inc) => 
+                    expect(query.includes(inc)).toBe(true)
+                );
+            }
+            else {
+                assert.fail("Call to queryDatabase with unexpected query " + query);
+            }
+        });
+        const result = await updateMolecule({
             id: 1,
             name: "voglibose",
             formula: "C10H21NO7",
             mass: 267.277,
             equivalents: [0],
             synonyms: ["vague", "vog"]
-            })
+        })
         expect(result).toBe(0);
-        expect(mockQuery).toHaveBeenCalledWith("SELECT * FROM view_complete_molecule WHERE id = $1", [1]);
-        expect(mockQuery).toHaveBeenCalledWith("INSERT INTO synonym (id_mol, name) VALUES ($1, $2)", [1, "vog"]);
     });
 
     test('modif molecule: delete synonym', async () => {
-        mockQuery.mockResolvedValueOnce({ rows: [{ 
-            id: 1,
-            name: "voglibose",
-            formula: "C10H21NO7",
-            mass: 267.277,
-            equivalent: [0],
-            synonym: ["vague", "vog"]
-        }] });
 
+        (queryDatabase as Mock).mockResolvedValueOnce({
+                    rows: [{
+                        id: 1,
+                        name: "voglibose",
+                        formula: "C10H21NO7",
+                        mass: 267.277,
+                        equivalent: [0],
+                        synonym: ["vague", "vog"]
+                    }]
+                });
+        (queryDatabase as Mock).mockResolvedValueOnce({ rows: [] });
+        (queryDatabase as Mock).mockResolvedValueOnce({ rows: [] });
+        (queryDatabase as Mock).mockImplementation((query: string,
+                                                    values: any[]) => {
+            if (query.includes("SELECT")
+                && query.includes("FROM view_complete_molecule")
+                && query.includes("WHERE id = $1")) {
+                expect(values).toContain([1]);
+            }
+            else if (query.includes("DELETE FROM") && query.includes("synonym")) {
+                expect(values).toContain([1]);
+                expect(query.includes("WHERE id_molecule = $1")).toBe(true)
+            }
+            else {
+                assert.fail("Call to queryDatabase with unexpected query " + query);
+            }
+        });
         const result = await updateMolecule({
             id: 1,
             name: "voglibose",
@@ -101,21 +171,44 @@ describe("molecule", async () => {
             mass: 267.277,
             equivalents: [0],
             synonyms: ["vague"]
-            })
+        })
         expect(result).toBe(0);
-        expect(mockQuery).toHaveBeenCalledWith("SELECT * FROM view_complete_molecule WHERE id = $1", [1]);
-        expect(mockQuery).toHaveBeenCalledWith("DELETE FROM synonym WHERE id_mol = $1 AND name = $2", [1, "vog"]);
     });
 
     test('modif molecule: add equivalent', async () => {
-        mockQuery.mockResolvedValueOnce({ rows: [{ 
-            id: 1,
-            name: "voglibose",
-            formula: "C10H21NO7",
-            mass: 267.277,
-            equivalent: [0],
-            synonym: ["vague"]
-        }] });
+        (queryDatabase as Mock).mockResolvedValueOnce({
+                    rows: [{
+                        id: 1,
+                        name: "voglibose",
+                        formula: "C10H21NO7",
+                        mass: 267.277,
+                        equivalent: [0],
+                        synonym: ["vague"]
+                    }]
+                });
+        (queryDatabase as Mock).mockResolvedValueOnce({ rows: [] });
+        (queryDatabase as Mock).mockResolvedValueOnce({ rows: [] });
+        (queryDatabase as Mock).mockImplementation((query: string,
+                                                    values: any[]) => {
+            if (query.includes("SELECT")
+                && query.includes("FROM view_complete_molecule")
+                && query.includes("WHERE id = $1")) {
+                expect(values).toContain([1]);
+            }
+            else if (query.includes("DELETE FROM") && query.includes("equivalent")) {
+                expect(values).toContain([1]);
+                expect(query.includes("WHERE id_molecule = $1")).toBe(true)
+            }
+            else if (query.includes("INSERT INTO") && query.includes("equivalent")) {
+                values.map((val) => expect([1, 2]).toContain(val));
+                ["id_mol_0", "id_mol_1"].map((inc) => 
+                    expect(query.includes(inc)).toBe(true)
+                );
+            }
+            else {
+                assert.fail("Call to queryDatabase with unexpected query " + query);
+            }
+        });
 
         const result = await updateMolecule({
             id: 1,
@@ -124,22 +217,43 @@ describe("molecule", async () => {
             mass: 267.277,
             equivalents: [0, 2],
             synonyms: ["vague"]
-            })
+        })
         expect(result).toBe(0);
-        expect(mockQuery).toHaveBeenCalledWith("SELECT * FROM view_complete_molecule WHERE id = $1", [1]);
-        expect(mockQuery).toHaveBeenCalledWith("INSERT INTO equivalent (id_mol_0, id_mol_1) VALUES ($1, $2)", [1, 2]);
     });
 
     test('modif molecule: delete equivalent', async () => {
-        mockQuery.mockResolvedValueOnce({ rows: [{ 
-            id: 1,
-            name: "voglibose",
-            formula: "C10H21NO7",
-            mass: 267.277,
-            equivalent: [0, 2],
-            synonym: ["vague"]
-        }] });
-
+        (queryDatabase as Mock).mockResolvedValueOnce({
+            rows: [{
+                id: 1,
+                name: "voglibose",
+                formula: "C10H21NO7",
+                mass: 267.277,
+                equivalent: [0, 2],
+                synonym: ["vague"]
+            }]
+        });
+        (queryDatabase as Mock).mockResolvedValueOnce({ rows: [] });
+        (queryDatabase as Mock).mockImplementation((query: string,
+                                                    values: any[]) => {
+            if (query.includes("SELECT")
+                && query.includes("FROM view_complete_molecule")
+                && query.includes("WHERE id = $1")) {
+                expect(values).toContain([1]);
+            }
+            else if (query.includes("DELETE FROM") && query.includes("equivalent")) {
+                expect(values).toContain([1]);
+                expect(query.includes("WHERE id_molecule = $1")).toBe(true)
+            }
+            else if (query.includes("INSERT INTO") && query.includes("equivalent")) {
+                values.map((val) => expect([1, 0]).toContain(val));
+                ["id_mol_0", "id_mol_1"].map((inc) => 
+                    expect(query.includes(inc)).toBe(true)
+                );
+            }
+            else {
+                assert.fail("Call to queryDatabase with unexpected query " + query);
+            }
+        });
         const result = await updateMolecule({
             id: 1,
             name: "voglibose",
@@ -147,9 +261,7 @@ describe("molecule", async () => {
             mass: 267.277,
             equivalents: [0],
             synonyms: ["vague"]
-            })
+        })
         expect(result).toBe(0);
-        expect(mockQuery).toHaveBeenCalledWith("SELECT * FROM view_complete_molecule WHERE id = $1", [1]);
-        expect(mockQuery).toHaveBeenCalledWith("DELETE FROM equivalent WHERE id_mol_0 = $1 AND id_mol_1 = $2 OR id_mol_0 = $2 AND id_mol_1 = $1", [1, 2]);
     });
 });
