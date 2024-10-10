3e66c5bcd9f102d5cb230910b13afa1cf090c200
M.Palerme <marcellino.palerme@inrae.fr>
2024-10-10 16:15:04 +0200

    Revert "migrate config eslint to flat"
    This reverts commit 888e46a0254abb46348e7b0c3c1f74353ce17c65.

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+// © 2024 INRAE
+// SPDX-FileContributor: Marcellino Palerme <marcellino.palerme@inrae.fr>
+//
+// SPDX-License-Identifier: MIT
+
+module.exports = {
+    root: true,
+    extends: ['@nuxt/eslint-config'],
+  }
